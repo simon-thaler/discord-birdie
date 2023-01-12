81f86f816db2de169657388bb72c9f3427941a46
Simon Thaler <tpfednr6vcjnzc5j@tutanota.com>
2023-01-12 09:36:15 -0600

    added sum shi

--- discord-birdie.py
+++ discord-birdie.py
@@ -12,15 +12,28 @@ driver = webdriver.Firefox()
 #open discord
 driver.get('https://discord.com/login')
 
-#await login
+#set wait driver time as 120 secconds
 wait = WebDriverWait(driver, 120)
+
+os.system('clear') #clear scrn
+print('You have 2 MINUTES to login to Discord.')
+print('*** Use the QR code to login easily ***')
+
+#initiate wait
 wait.until(EC.title_contains("Discord | Friends"))
+
 
 #get sever group web element
 server_list = driver.find_element(By.XPATH, "//div[@aria-label='Servers']")
-servers = server_list.find_element(By.XPATH, "./child::*")
+servers = server_list.find_elements(By.CLASS_NAME, "blobContainer-ikKyFs")
 
-for i in servers:
-    print(i)
-    
+
+print("SELECT A SERVER BELOW")
+print("---------------------")
+print_ser = 0
+while print_ser-1 != servers.__len__:
+    print(print_ser, '. ', servers[print_ser-1].get_attribute("data-dnd-name"))
+    print_ser = print_ser + 1
+
+
 #going to sleep for tonight
